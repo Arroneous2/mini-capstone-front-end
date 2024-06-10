5edd1cbe0b4d464b9464dbeecd16d5e142973795
Arroneous2 <thumpinbassbeat@gmail.com>
2024-06-10 12:45:07 -0500

    starting app

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,18 @@
+import { ProductsIndex } from "./ProductsIndex";
+
 export function Content() {
+  const products = [
+    {
+      name: "first",
+      price: 10,
+      image_url: "https://www.google.com/images/branding/googlelogo/2x/googlelogo_light_color_92x30dp.png",
+      description: "lots of text here now",
+    },
+  ];
+
   return (
-    <main>
-      <h1>Welcome to React!</h1>
-    </main>
+    <div>
+      <ProductsIndex products={products} />
+    </div>
   );
 }
